75249dd40918af7f68a9713fcc28db702f1217a7
Rishabh <rishabh03082000@gmail.com>
2025-02-20 00:59:24 +0530

    Implemented date time logic-
    1. Doctor is able to define his available hours
    2. Time slot class is modified to parse input date-time string to start and end time object

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,55 @@ import mode.ConsolePrint;
 import mode.Print;
 import model.Doctor;
 import model.Speciality;
+import model.TimeSlot;
 import repository.DoctorRepository;
 import repository.PatientsRepository;
 import service.DoctorService;
 import service.PatientService;
 
+import javax.print.Doc;
+import java.sql.Time;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Print print = new ConsolePrint();
-        PatientsRepository patientsRepository=new PatientsRepository();
-        DoctorRepository doctorRepository=new DoctorRepository();
-        DoctorService doctorService=new DoctorService(print,doctorRepository);
-        PatientService patientService = new PatientService(print,patientsRepository);
+        PatientsRepository patientsRepository = new PatientsRepository();
+        DoctorRepository doctorRepository = new DoctorRepository();
+        DoctorService doctorService = new DoctorService(print, doctorRepository);
+        PatientService patientService = new PatientService(print, patientsRepository);
 
         // Doctors
-        Doctor doctorCurious = new Doctor(1, "curious",new HashMap<>(), Speciality.Cardiologist);
-        Doctor doctorDreadful = new Doctor(2, "Dreadful",new HashMap<>(), Speciality.Dermatologist);
-        Doctor doctorDaring = new Doctor(3, "Daring",new HashMap<>(), Speciality.Dermatologist);
+        Scanner sc = new Scanner(System.in);
 
-        doctorService.registerDoc(doctorCurious);
+        Doctor doctor = getTheDoctorDetails(sc);
+
+        doctorService.registerDoc(doctor);
+    }
+
+    public static Doctor getTheDoctorDetails(Scanner sc) {
+        System.out.print("Enter Doctor ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        // Consume newline
+        System.out.println("Enter Doctor name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter Speciality (Cardiologist/Dermatologist/Other): ");
+        Speciality speciality = Speciality.valueOf(sc.nextLine());
+        HashMap<TimeSlot, Boolean> availableTimeSlot = new HashMap<>();
+        System.out.println("Enter available time slots (e.g., 09:00-09:30), type 'done' to finish:");
+        while (true) {
+            String timeInput = sc.nextLine();
+            if (timeInput.equalsIgnoreCase("done")) break;
+            try {
+                TimeSlot timeSlot = TimeSlot.parse(timeInput);
+                availableTimeSlot.put(timeSlot, true);
+            } catch (Exception e) {
+                System.out.println("Invalid format! Please enter in HH:mm-HH:mm format (30-minute intervals).");
+            }
+        }
+        return new Doctor(id, name, availableTimeSlot, speciality);
     }
 }
--- src/model/TimeSlot.java
+++ src/model/TimeSlot.java
@@ -1,45 +1,59 @@
 package model;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class TimeSlot {
-    String start;
-    String end;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private static final DateTimeFormatter FORMATTER=DateTimeFormatter.ofPattern("HH:mm");
 
-    public String getStart() {
-        return start;
+    //Constructor
+    public TimeSlot(LocalTime startTime,LocalTime endTime){
+        if(!startTime.plusMinutes(30).equals(endTime)){
+            throw new IllegalArgumentException("Time slot should be of 30 mins");
+        }
+        this.startTime=startTime;
+        this.endTime=endTime;
+    }
+    //Parsing input String (HH:MM-HH:MM) to Datetime format/TimeSlot Object.
+    public static TimeSlot parse(String timeRange){
+        String[] times=timeRange.split("-");
+        if(times.length!=2) throw new IllegalArgumentException("Invalid time format! Use HH:mm-HH:mm");
+
+        LocalTime start=LocalTime.parse(times[0].trim(),FORMATTER); //converting input to required format
+        LocalTime end=LocalTime.parse(times[1].trim(),FORMATTER); //converting input to required format
+
+        return new TimeSlot(start,end);
     }
 
-    public void setStart(String start) {
-        this.start = start;
+    public LocalTime getStartTime() {
+        return startTime;
     }
 
-    public String getEnd() {
-        return end;
-    }
-
-    public void setEnd(String end) {
-        this.end = end;
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        TimeSlot that = (TimeSlot) o;
-        return Objects.equals(start, that.start) && Objects.equals(end, that.end);
+        TimeSlot timeSlot = (TimeSlot) o;
+        return Objects.equals(startTime, timeSlot.startTime) && Objects.equals(endTime, timeSlot.endTime);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(start, end);
+        return Objects.hash(startTime, endTime);
     }
 
     @Override
     public String toString() {
-        return "AvailableTime{" +
-                "start='" + start + '\'' +
-                ", end='" + end + '\'' +
+        return "TimeSlot{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
                 '}';
     }
 }
